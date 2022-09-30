501ee3484afb574bed3e49d8e3bacc9b47919149
RayPuig <raypuig@gmail.com>
2022-09-30 09:00:06 +0200

    actualitzat

--- R/dataFormatGe.R
+++ R/dataFormatGe.R
@@ -1,15 +1,12 @@
-#' @title  DATA RANDOM ENTRE DUES DATES (dataini i datafi)
-#' @description DATA RANDOM ENTRE DUES DATES 
-#' @param  dataini           xxx
-#' @param  datafi            xxx
-#' @return variables
+#' @title Data Random entre dues dates 
+#' @description Data Random entre dues dates (dataini i datafi) 
+#' @param  dataini data inicial
+#' @param  datafi  data final
+#' @return una data random
 #' @export data.random
-#' @importFrom dplyr "%>%"
 #' @examples
-#' domini="farmacs_prescrits"
-#' cod=c("A10BB01","A10BD01","A10BD04","A10BA02","J01DD07")
-#' agr_Farmac=c("Sulfonilureas","Biguanidas","Tiazolidinadiones","Biguanidas","Antibioticos")
-#' dt_cataleg<-data.frame(domini=domini,cod=cod,agr_Farmac=agr_Farmac)
+#' data<-data.random(20120101,20121231)
+#' data
 data.random <- function(dataini=20120101, datafi=20121231) {
   
   # dataini=20120101
@@ -24,12 +21,11 @@ data.random <- function(dataini=20120101, datafi=20121231) {
 }
 
 
-#' @title   RETORNA UNA DATA A STRING
-#' @description RETORNA UNA DATA A STRING 
-#' @param  data           xxx
-#' @return RETORNA UNA DATA A STRING 
+#' @title Retorna una data a STRING
+#' @description Retorna una data a STRING 
+#' @param  data una data     
+#' @return data a STRING 
 #' @export data.to.string
-#' @importFrom dplyr "%>%"
 #' @examples
 #' A<-"27-09-2022"
 #' B<-data.to.string(A)
@@ -44,19 +40,20 @@ data.to.string<-function(data) {
 
 
 
+
+
 #' @title   Data R Lubridate a partir de data UTC
 #' @description Data R Lubridate a partir de data UTC 
-#' @param  x             xxx
-#' @param  dt            xxx
-#' @return variables
+#' @param  x variablesde data
+#' @param  dt base de dades
+#' @return base de dades amb Data R Lubridate
 #' @export dataUTC_to_Rdata
 #' @importFrom dplyr "%>%"
 #' @examples
 #'
-#' domini="farmacs_prescrits"
-#' cod=c("A10BB01","A10BD01","A10BD04","A10BA02","J01DD07")
-#' agr_Farmac=c("Sulfonilureas","Biguanidas","Tiazolidinadiones","Biguanidas","Antibioticos")
-#' dt_cataleg<-data.frame(domini=domini,cod=cod,agr_Farmac=agr_Farmac)
+#'dt_plana
+#'x=c("dtindex","dnaix","entrada","sortida", "INCLUSIO.DM2")
+#'dataUTC_to_Rdata(x,dt_plana)
 dataUTC_to_Rdata<-function(x,dt) {
   
   # dt<-dades
@@ -70,7 +67,6 @@ dataUTC_to_Rdata<-function(x,dt) {
   
   # Vector de variables caracter ("37712")
   x_text<-x [x %in% vector_caracter]
-  
   
   # Funcio que converteix UTC data a date ymd
   data_convert_UTC<-function(x){
@@ -92,69 +88,58 @@ dataUTC_to_Rdata<-function(x,dt) {
 }
 
 
-
-
-#' @title   converteix data caracter ("37712") a date ymd
-#' @description Funcio que converteix data caracter ("37712") a date ymd () "2003-04-01"
-#' @param  x            xxx
-#' @return variables
+#' @title Converteix data caracter () a una data ymd
+#' @description Converteix data caracter ("37712") a date ymd () "2003-04-01"
+#' @param  x data caracter
+#' @return data ymd
 #' @export data_convert_text
 #' @importFrom dplyr "%>%"
 #' @examples
-#' domini="farmacs_prescrits"
-#' cod=c("A10BB01","A10BD01","A10BD04","A10BA02","J01DD07")
-#' agr_Farmac=c("Sulfonilureas","Biguanidas","Tiazolidinadiones","Biguanidas","Antibioticos")
-#' dt_cataleg<-data.frame(domini=domini,cod=cod,agr_Farmac=agr_Farmac)
+#' k<-data_convert_text("37712")
+#' k
 data_convert_text<-function(x){
   x<-as.Date(as.numeric(x), origin = "1899-12-30") %>% 
     lubridate::ymd()}
 
  
 
-#' @title   Funcio que converteix de numeric (15784) a Date
+#' @title Funcio que converteix de una data numeric (15784) a Date
 #' @description Funcio que converteix de numeric (15784) a Date "2013-03-20" 
-#' @param  x            xxx
-#' @return data
+#' @param  x data numeric
+#' @return Date "2013-03-20" 
 #' @export data_convert_numeric
 #' @importFrom dplyr "%>%"
 #' @examples
-#' domini="farmacs_prescrits"
-#' cod=c("A10BB01","A10BD01","A10BD04","A10BA02","J01DD07")
-#' agr_Farmac=c("Sulfonilureas","Biguanidas","Tiazolidinadiones","Biguanidas","Antibioticos")
-#' dt_cataleg<-data.frame(domini=domini,cod=cod,agr_Farmac=agr_Farmac)
+#'k<-data_convert_numeric(15784)
+#'k
 data_convert_numeric<-function(x){ x<-as.Date(x, origin = "1970-01-01")}
+
 
 
 
 #' @title   Funcio que converteix UTC data a date ymd
 #' @description Funcio que converteix UTC data a date ymd 
-#' @param  x            xxx
-#' @return data
+#' @param  x UTC data
+#' @return date ymd
 #' @export data_convert_UTC
-#' @importFrom dplyr "%>%"
 #' @examples
-#' domini="farmacs_prescrits"
-#' cod=c("A10BB01","A10BD01","A10BD04","A10BA02","J01DD07")
-#' agr_Farmac=c("Sulfonilureas","Biguanidas","Tiazolidinadiones","Biguanidas","Antibioticos")
-#' dt_cataleg<-data.frame(domini=domini,cod=cod,agr_Farmac=agr_Farmac)
+#'k<-data_convert_UTC("1970-01-01")
+#'k
 data_convert_UTC<-function(x){
   x<-format(as.POSIXct(x, origin='1970-01-01'), format='%Y/%m/%d')
   x<-lubridate::ymd(x)}
 
 
-#' @title   CONVERTEIX FORMAT TEXT A DATA 
-#' @description Format YYYYMMDD (Format text -> data)   
-#' @param  d                         xxx
-#' @param  taulavariables            xxx
-#' @param  campdata                  xxx
+
+
+#' @title Converteix format TEXT A DATA 
+#' @description Converteix format TEXT A DATA (Format text -> data)   
+#' @param  d                         base de dades
+#' @param  taulavariables            variables que volem canviar
+#' @param  campdata                  dates
 #' @return data
 #' @export convertir_dates
 #' @importFrom dplyr "%>%"
-#' @examples
-#' domini="farmacs_prescrits"
-#' cod=c("A10BB01","A10BD01","A10BD04","A10BA02","J01DD07")
-#' agr_Farmac=c("Sulfonilureas","Biguanidas","Tiazolidinadiones","Biguanidas","Antibioticos")
-#' dt_cataleg<-data.frame(domini=domini,cod=cod,agr_Farmac=agr_Farmac)
 convertir_dates<-function(d="dadestotal",taulavariables="variables_R.xls",campdata="dates")
   
 {
@@ -183,18 +168,17 @@ convertir_dates<-function(d="dadestotal",taulavariables="variables_R.xls",campda
 
 
 
-# 13481683200 --> 2010-01-01
+
+
 #' @title   Passa data de SPSS a Rdata
 #' @description Passa data de SPSS a Rdata 
-#' @param  x            xxx
-#' @return data
-#' @export dataUTC_to_Rdata
+#' @param  x data de SPSS
+#' @return data Rdata
+#' @export dataSPSS_to_Rdata
 #' @importFrom dplyr "%>%"
 #' @examples
-#' domini="farmacs_prescrits"
-#' cod=c("A10BB01","A10BD01","A10BD04","A10BA02","J01DD07")
-#' agr_Farmac=c("Sulfonilureas","Biguanidas","Tiazolidinadiones","Biguanidas","Antibioticos")
-#' dt_cataleg<-data.frame(domini=domini,cod=cod,agr_Farmac=agr_Farmac)
+#'k<-dataSPSS_to_Rdata(666)
+#'k
 dataSPSS_to_Rdata <- function(x) {
   y<-as.Date(x/86400, origin = "1582-10-14") %>% 
     lubridate::ymd() }
