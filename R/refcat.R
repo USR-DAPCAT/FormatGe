1ca897398e253e6fb8923fdd9aa5f0c61d989c29
Ramon Puig Treserres <raypuig@gmail.com>
2022-11-15 12:23:08 +0100

    act

--- R/refcat.R
+++ R/refcat.R
@@ -12,11 +12,43 @@ refcat<-function(DF="iris",conductor="conductor_iris",ref="ref_cat",...){
   # conductor=conductor_matlab
   # ref="ref_cat"
   
+  #https://search.r-project.org/CRAN/refmans/forcats/html/fct_relevel.html
+  
+  #exclusio=c("","","","","","","","","","","","","","",">30")
+  #exc_ordre=c("","","","","","","","","","","","","","","1")
+  #exc_pre=c("","","","","","","","","","","","","","","")
+  #ref_cat=c("","","H","","","","","","","","","","","","")
+  #conductor2<-data.frame(conductor1,exclusio,exc_ordre,exc_pre,ref_cat)
+  #exclusio1<-c("exclusio1","exclusio1","exclusio1","","","","","","","","","","","1","")
+  #conductor2<-rbind(conductor2,exclusio1)
+  #
+  #
+  #DF=Taula_plana
+  #conductor=conductor2
+  #ref="ref_cat"
+  
+  
+  # Create a factor with the wrong order of levels
+  #sizes <- factor(c("small", "large", "large", "small", "medium"))
+  #sizes
+  #> [1] small  large  large  small  medium
+  #> Levels: large medium small
+  
+  #sizes <- factor(sizes, levels=rev(levels(sizes)))
+  #sizes
+  #> [1] small  large  large  small  medium
+  #> Levels: small medium large
+  
+  
+  
   
     
     ref=rlang::sym(ref)
     # llegeixo conductor informacio de refcats
-    conductor_df<-read_conductor(conductor,...) %>%dplyr:: select(camp,!!ref) %>% dplyr::filter(!!ref!="") 
+    
+    #conductor_df<-read_conductor(conductor,...) %>%dplyr:: select(camp,!!ref) %>% dplyr::filter(!!ref!="") 
+    conductor_df<-read_conductor(conductor) %>%dplyr:: select(camp,!!ref) %>% dplyr::filter(!!ref!="") 
+    
     llista_vars<-conductor_df$camp %>% as.character()
     
     # Factoritzar variables i verificar nivells
@@ -44,14 +76,16 @@ refcat<-function(DF="iris",conductor="conductor_iris",ref="ref_cat",...){
     llista_refcat<-conductor_df %>% dplyr::pull(!!ref) 
     
     # Faig el relevel a les comlumnes seleccionades
-    pp<-purrr::map2_df(DF %>% dplyr::select(llista_vars),  llista_refcat, ~stats::relevel(.x, .y))
+    pp<-purrr::map2_df(DF %>% dplyr::select(llista_vars),  llista_refcat, ~ forcats::fct_relevel(stats::relevel(.x, .y)),sort)
     
     # Ara intercanviar columnes 2 a 2 de
     DF[llista_vars]<-pp[llista_vars]
     DF
     
+    #levels(DF$sexe)
     # ara falta ordenar-ho ....ex::conductor_HTG [HTG_Cohort]
     
+    #fct_relevel(f, sort)
   
 }
 
